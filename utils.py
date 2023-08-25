3e71dc1040f67672c5606ab6551c43ef05136d9e
David Warutumo <davidwarchy@gmail.com>
2023-08-25 18:27:23 +0300

    output to excel

--- utils.py
+++ utils.py
@@ -49,8 +49,43 @@ def read_top_100_rows_with_qid():
         rows = cursor.fetchall()
         return rows
 
+import pandas as pd
+import sqlite3
+
+def export_sqlite_to_excel(data, excel_filename):
+    """
+    Export SQLite data to an Excel file.
+    
+    :param data: List of tuples containing the data to export
+    :param excel_filename: Name of the Excel file to create
+    """
+    # Create a DataFrame from the data
+    df = pd.DataFrame(data, columns=["title", "subject", "qid", "qidDescription"])
+    
+    # Write DataFrame to Excel
+    df.to_excel(excel_filename, index=False)
+
+def read_top_100_rows_with_qid():
+    """
+    Read and return the top 100 rows from the destination table where qid is not None, null, or empty
+    """
+    query = """
+            SELECT title, subject, qid, qidDescription
+            FROM destination
+            WHERE qid IS NOT NULL AND qid != ''
+            LIMIT 100;
+            """
+    with get_db_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        return rows
+
+# Assuming you have a function get_db_connection() that returns a database connection
+# You can call the read_top_100_rows_with_qid() function to get the data and then export it to Excel
+
+
 # Example usage
 if __name__ == "__main__":
-    rows = read_top_100_rows_with_qid()
-    for row in rows:
-        print(row)
+    data = read_top_100_rows_with_qid()
+    export_sqlite_to_excel(data, "output.xlsx")
